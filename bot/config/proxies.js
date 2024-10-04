745e4d98447757779d434248a51d87ad97cde226
Pourya <p.dadsetan7370@gmail.com>
2024-10-04 22:41:12 +0330

    'init'

---
+++ bot/config/proxies.js
@@ -0,0 +1,487 @@
+module.exports = [
+  {
+    "ip": "79.127.35.243",
+    "port": 5678,
+    "protocol": "http"
+  },
+  {
+    "ip": "154.113.86.193",
+    "port": 5678,
+    "protocol": "http"
+  },
+  {
+    "ip": "159.192.143.241",
+    "port": 4153,
+    "protocol": "http"
+  },
+  {
+    "ip": "103.248.30.2",
+    "port": 5678,
+    "protocol": "http"
+  },
+  {
+    "ip": "5.9.149.69",
+    "port": 24940,
+    "protocol": "http"
+  },
+  {
+    "ip": "103.213.245.105",
+    "port": 4003,
+    "protocol": "http"
+  },
+  {
+    "ip": "5.189.133.192",
+    "port": 50893,
+    "protocol": "http"
+  },
+  {
+    "ip": "188.255.244.9",
+    "port": 1080,
+    "protocol": "http"
+  },
+  {
+    "ip": "187.19.127.253",
+    "port": 4153,
+    "protocol": "http"
+  },
+  {
+    "ip": "154.113.161.1",
+    "port": 5678,
+    "protocol": "http"
+  },
+  {
+    "ip": "119.148.37.198",
+    "port": 1088,
+    "protocol": "http"
+  },
+  {
+    "ip": "68.183.17.152",
+    "port": 8016,
+    "protocol": "http"
+  },
+  {
+    "ip": "203.24.108.231",
+    "port": 80,
+    "protocol": "http"
+  },
+  {
+    "ip": "104.23.126.8",
+    "port": 80,
+    "protocol": "http"
+  },
+  {
+    "ip": "157.185.170.32",
+    "port": 26589,
+    "protocol": "http"
+  },
+  {
+    "ip": "88.135.42.241",
+    "port": 4153,
+    "protocol": "http"
+  },
+  {
+    "ip": "201.33.161.234",
+    "port": 4153,
+    "protocol": "http"
+  },
+  {
+    "ip": "124.121.95.244",
+    "port": 8080,
+    "protocol": "http"
+  },
+  {
+    "ip": "123.25.116.228",
+    "port": 1080,
+    "protocol": "http"
+  },
+  {
+    "ip": "124.133.20.234",
+    "port": 7302,
+    "protocol": "http"
+  },
+  {
+    "ip": "38.127.179.10",
+    "port": 11537,
+    "protocol": "http"
+  },
+  {
+    "ip": "192.12.113.226",
+    "port": 4145,
+    "protocol": "http"
+  },
+  {
+    "ip": "149.28.122.98",
+    "port": 46560,
+    "protocol": "http"
+  },
+  {
+    "ip": "181.3.75.194",
+    "port": 1080,
+    "protocol": "http"
+  },
+  {
+    "ip": "110.78.146.228",
+    "port": 4145,
+    "protocol": "http"
+  },
+  {
+    "ip": "64.119.30.126",
+    "port": 5678,
+    "protocol": "http"
+  },
+  {
+    "ip": "41.242.90.3",
+    "port": 57520,
+    "protocol": "http"
+  },
+  {
+    "ip": "69.63.73.234",
+    "port": 5678,
+    "protocol": "http"
+  },
+  {
+    "ip": "185.243.126.53",
+    "port": 4191,
+    "protocol": "http"
+  },
+  {
+    "ip": "104.21.85.109",
+    "port": 80,
+    "protocol": "http"
+  },
+  {
+    "ip": "1.20.220.79",
+    "port": 4145,
+    "protocol": "http"
+  },
+  {
+    "ip": "59.59.51.74",
+    "port": 8001,
+    "protocol": "http"
+  },
+  {
+    "ip": "138.117.179.54",
+    "port": 5678,
+    "protocol": "http"
+  },
+  {
+    "ip": "181.66.37.143",
+    "port": 4153,
+    "protocol": "http"
+  },
+  {
+    "ip": "177.55.57.69",
+    "port": 3128,
+    "protocol": "http"
+  },
+  {
+    "ip": "185.89.156.130",
+    "port": 5678,
+    "protocol": "http"
+  },
+  {
+    "ip": "43.157.32.4",
+    "port": 443,
+    "protocol": "http"
+  },
+  {
+    "ip": "41.60.234.231",
+    "port": 8080,
+    "protocol": "http"
+  },
+  {
+    "ip": "181.3.93.211",
+    "port": 1080,
+    "protocol": "http"
+  },
+  {
+    "ip": "106.75.174.172",
+    "port": 999,
+    "protocol": "http"
+  },
+  {
+    "ip": "172.67.14.237",
+    "port": 80,
+    "protocol": "http"
+  },
+  {
+    "ip": "177.128.44.129",
+    "port": 31337,
+    "protocol": "http"
+  },
+  {
+    "ip": "185.162.128.135",
+    "port": 9057,
+    "protocol": "http"
+  },
+  {
+    "ip": "171.97.107.187",
+    "port": 4145,
+    "protocol": "http"
+  },
+  {
+    "ip": "2.137.22.252",
+    "port": 4153,
+    "protocol": "http"
+  },
+  {
+    "ip": "162.38.140.6",
+    "port": 8080,
+    "protocol": "http"
+  },
+  {
+    "ip": "181.188.187.139",
+    "port": 5678,
+    "protocol": "http"
+  },
+  {
+    "ip": "187.94.105.30",
+    "port": 4153,
+    "protocol": "http"
+  },
+  {
+    "ip": "123.30.149.92",
+    "port": 8444,
+    "protocol": "http"
+  },
+  {
+    "ip": "202.150.1.87",
+    "port": 80,
+    "protocol": "http"
+  },
+  {
+    "ip": "117.111.251.102",
+    "port": 8081,
+    "protocol": "http"
+  },
+  {
+    "ip": "202.5.54.70",
+    "port": 4145,
+    "protocol": "http"
+  },
+  {
+    "ip": "79.143.190.221",
+    "port": 12580,
+    "protocol": "http"
+  },
+  {
+    "ip": "1.1.1.177",
+    "port": 80,
+    "protocol": "http"
+  },
+  {
+    "ip": "185.157.109.5",
+    "port": 5678,
+    "protocol": "http"
+  },
+  {
+    "ip": "95.79.111.39",
+    "port": 1080,
+    "protocol": "http"
+  },
+  {
+    "ip": "45.7.177.10",
+    "port": 52246,
+    "protocol": "http"
+  },
+  {
+    "ip": "181.101.1.221",
+    "port": 7071,
+    "protocol": "http"
+  },
+  {
+    "ip": "157.245.59.71",
+    "port": 42765,
+    "protocol": "http"
+  },
+  {
+    "ip": "102.64.116.97",
+    "port": 4145,
+    "protocol": "http"
+  },
+  {
+    "ip": "195.93.200.140",
+    "port": 80,
+    "protocol": "http"
+  },
+  {
+    "ip": "103.239.200.226",
+    "port": 1337,
+    "protocol": "http"
+  },
+  {
+    "ip": "104.20.198.49",
+    "port": 80,
+    "protocol": "http"
+  },
+  {
+    "ip": "172.64.80.55",
+    "port": 80,
+    "protocol": "http"
+  },
+  {
+    "ip": "43.153.11.102",
+    "port": 443,
+    "protocol": "http"
+  },
+  {
+    "ip": "104.16.105.207",
+    "port": 80,
+    "protocol": "http"
+  },
+  {
+    "ip": "101.43.3.207",
+    "port": 2080,
+    "protocol": "http"
+  },
+  {
+    "ip": "157.245.62.1",
+    "port": 9050,
+    "protocol": "http"
+  },
+  {
+    "ip": "178.62.223.104",
+    "port": 80,
+    "protocol": "http"
+  },
+  {
+    "ip": "77.235.28.229",
+    "port": 4153,
+    "protocol": "http"
+  },
+  {
+    "ip": "124.121.176.254",
+    "port": 4145,
+    "protocol": "http"
+  },
+  {
+    "ip": "179.99.202.210",
+    "port": 4153,
+    "protocol": "http"
+  },
+  {
+    "ip": "197.234.13.19",
+    "port": 4145,
+    "protocol": "http"
+  },
+  {
+    "ip": "110.169.175.89",
+    "port": 4145,
+    "protocol": "http"
+  },
+  {
+    "ip": "35.190.107.16",
+    "port": 30000,
+    "protocol": "http"
+  },
+  {
+    "ip": "103.83.105.167",
+    "port": 4153,
+    "protocol": "http"
+  },
+  {
+    "ip": "1.179.194.137",
+    "port": 4153,
+    "protocol": "http"
+  },
+  {
+    "ip": "36.95.14.243",
+    "port": 5678,
+    "protocol": "http"
+  },
+  {
+    "ip": "149.154.69.203",
+    "port": 3080,
+    "protocol": "http"
+  },
+  {
+    "ip": "168.138.162.66",
+    "port": 1080,
+    "protocol": "http"
+  },
+  {
+    "ip": "110.34.166.182",
+    "port": 4153,
+    "protocol": "http"
+  },
+  {
+    "ip": "186.1.7.13",
+    "port": 4153,
+    "protocol": "http"
+  },
+  {
+    "ip": "103.87.212.15",
+    "port": 4153,
+    "protocol": "http"
+  },
+  {
+    "ip": "217.150.40.189",
+    "port": 3128,
+    "protocol": "http"
+  },
+  {
+    "ip": "212.3.109.7",
+    "port": 4145,
+    "protocol": "http"
+  },
+  {
+    "ip": "81.162.199.169",
+    "port": 34955,
+    "protocol": "http"
+  },
+  {
+    "ip": "177.38.182.166",
+    "port": 5678,
+    "protocol": "http"
+  },
+  {
+    "ip": "36.255.104.9",
+    "port": 1080,
+    "protocol": "http"
+  },
+  {
+    "ip": "118.218.126.54",
+    "port": 9400,
+    "protocol": "http"
+  },
+  {
+    "ip": "177.72.113.225",
+    "port": 31164,
+    "protocol": "http"
+  },
+  {
+    "ip": "85.25.93.172",
+    "port": 5566,
+    "protocol": "http"
+  },
+  {
+    "ip": "197.234.13.17",
+    "port": 4145,
+    "protocol": "http"
+  },
+  {
+    "ip": "104.17.166.210",
+    "port": 80,
+    "protocol": "http"
+  },
+  {
+    "ip": "113.86.204.187",
+    "port": 44844,
+    "protocol": "http"
+  },
+  {
+    "ip": "45.77.77.144",
+    "port": 11304,
+    "protocol": "http"
+  },
+  {
+    "ip": "181.88.73.214",
+    "port": 5678,
+    "protocol": "http"
+  },
+  {
+    "ip": "168.227.158.85",
+    "port": 4145,
+    "protocol": "http"
+  }
+];
